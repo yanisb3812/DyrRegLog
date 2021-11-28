530912fd40faac430f16e4886a9058fb07ccf00b
yanisb3812 <72911055+yanisb3812@users.noreply.github.com>
2021-11-28 11:11:29 +0100

    Add files via upload

--- R/calcul_gradient.R
+++ R/calcul_gradient.R
@@ -1,21 +1,25 @@
 #' Calcul de Gradient
 #' Une fonction pour calculer une regression logistique avec l'algorithme de la descente de gradient stochastique
 #'
-#' @param X,nb_rows,Erreurs_prediction,Type oui oui
+#' @param X,Type,nb_rows oui oui
 #'
 #' @return la fonction renvoie divers éléments
 #'
 #' @export
-calcul_Gradient <- function(X, nb_rows, Erreurs_prediction, Type){
-  X = as.matrix(as.data.frame(X))
+calcul_Gradient <- function(X,Type,nb_rows){ #Erreurs_prediction, nb_rows
 
   #Si on est sur du online nous avons un vecteur, sinon nous on avons une matrice
   if(Type=="Online"){
+    Erreurs_prediction = X[length(X)]
+    X = X[-length(X)]
     Erreurs_prediction=as.vector(Erreurs_prediction)
     gradient_values = (1/nb_rows)*(X*Erreurs_prediction)
   }else{
+    Erreurs_prediction = X[,ncol(X)]
+    X = as.matrix(as.data.frame(X[,-ncol(X)]))
     gradient_values = (1/nb_rows)*(t(X)%*%Erreurs_prediction)
   }
 
   return(gradient_values)
 }
+
