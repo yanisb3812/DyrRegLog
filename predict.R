fb67313197fe31d0a5980da7bef2a605e11ae108
Rayan Moissonnier <92780544+RayanMoissonnier@users.noreply.github.com>
2021-11-29 14:51:00 +0100

    Add files via upload

---
+++ predict.R
@@ -0,0 +1,35 @@
+#' Logistic Regression
+#'
+#' A function to predict classes using the logistic regression with the stochastic gradient descent algorithm.
+#'
+#' @param data_test A test data frame.
+#' @param Train_res Fitted model from data train using logistic regression.
+#' @param sortie "Proba" : return probabilities or "Class" : return classes predicted on test data.
+#'
+#' @return predict function returns a Vector of probabilities or classes predicted on test data.
+#'
+#' @export
+predict <-function(data_test, Train_res,sortie="Class"){
+  #PREDICTION : Y_Chapeau = 1/(1+exp(f(w,b)))
+  #f(w,b) => linear_Model
+  
+  
+  for (i in 1:dim(data_test)[2]){
+    data_test[,i]=(data_test[,i]-Train_res$mean_var[i])/Train_res$sd_var[i]
+  }
+  data_test = as.matrix(data_test)
+
+  vect_y_predits = -1*((data_test %*% Train_res$vect_Poids)) + Train_res$Biais # -Wx + B
+  Y_Predicted_Probas = 1/(1+exp(vect_y_predits)) #Liste des probabilites
+  Y_Predicted_Classes = round(Y_Predicted_Probas)
+
+  Y_Predicted_Probas=as.vector(Y_Predicted_Probas)
+  Y_Predicted_Classes=as.vector(Y_Predicted_Classes)
+  if (sortie=="Class"){
+    return(Y_Predicted_Classes)
+  } else if (sortie=="Proba"){
+    return(Y_Predicted_Probas)
+  } else {
+    stop("Error : set Sortie = 'Proba' or 'Class'")
+  }
+}
